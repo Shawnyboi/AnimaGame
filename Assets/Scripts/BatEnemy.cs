2149e406b2a12fb7e48d69520733a49c8682a04f
shawnyboi1 <shawnmhayes111@gmail.com>
2018-11-09 21:17:23 -0500

    Started Enemy Behavior Programming
    -Wrote StateMachine Class for enemies
    -Made very basic bat enemy

---
+++ Assets/Scripts/BatEnemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatEnemy : Enemy {
+
+    public float m_IdleTime = 1f;
+    public float m_StrollTime = 2f;
+
+    protected Vector2 m_Direction;
+    protected float m_StateTimer = 0f;
+    protected Transform m_Transform;
+
+    protected override void InitializeEnemy()
+    {
+        m_Transform = GetComponent<Transform>();
+        m_Direction = GetRandomVector();
+    }
+
+    protected override EnemyState Idle()
+    {
+        m_StateTimer += Time.deltaTime;
+        if (m_StateTimer > m_IdleTime)
+        {
+            m_StateTimer = 0f;
+            m_Direction = GetRandomVector();
+            return EnemyState.STROLL;
+        }
+        else
+        {
+            return EnemyState.IDLE;
+        }
+    }
+
+    protected override EnemyState Stroll()
+    {
+        m_StateTimer += Time.deltaTime;
+        if(m_StateTimer > m_StrollTime)
+        {
+            m_StateTimer = 0f;
+            return EnemyState.IDLE;
+        }
+        else
+        {
+            MoveInDirection(m_Direction);
+            return EnemyState.STROLL;
+        }
+    }
+
+    protected void MoveInDirection(Vector2 dir)
+    {
+        m_Transform.Translate(dir * Time.deltaTime);
+    }
+
+    protected Vector2 GetRandomVector()
+    {
+        float randRot = Random.Range(0f, 360f);
+        return new Vector2(Mathf.Cos(randRot), Mathf.Sin(randRot));
+    }
+
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour {
+
+    public enum EnemyState { IDLE, ATTACK, STROLL };
+
+    protected StateMachine<EnemyState> m_StateMachine;
+
+    protected abstract void InitializeEnemy();
+
+    protected virtual void Start()
+    {
+        InitializeEnemy();
+    }
+
+    public Enemy()
+    {
+        m_StateMachine = new StateMachine<EnemyState>();
+        m_StateMachine[EnemyState.IDLE] = Idle;
+        m_StateMachine[EnemyState.ATTACK] = Attack;
+        m_StateMachine[EnemyState.STROLL] = Stroll;
+    }
+
+    virtual protected EnemyState Idle()
+    {
+        Debug.Log("Idling");
+        return EnemyState.IDLE;
+    }
+    
+    virtual protected EnemyState Attack()
+    {
+        Debug.Log("Attacking");
+        return EnemyState.ATTACK;
+    }
+
+    virtual protected EnemyState Stroll()
+    {
+        Debug.Log("Strolling");
+        return EnemyState.STROLL;
+    }
+
+    virtual protected void Update()
+    {
+        m_StateMachine.Update();
+    }
+
+
+}
