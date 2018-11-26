e8944fd1b485c664fa757f21b205e0a98f82ae53
Ivan Lavrentyev <lavrentyev.ivan@gmail.com>
2018-11-25 22:22:03 -0500

    Player Animation
    Created Player Sprities, Animation, Animation Controller, and Animation Override Controller. Created script to change player direction with input (might need to be changed later). Also imported perfect pixel camera script package.

---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour {
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        ResetAllTriggers();
+        if(Mathf.Abs(h) > 0.1 || Mathf.Abs(v) > 0.1)
+        {
+            animator.SetBool("isMoving", true);
+        }else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        SetDirection(h, v);
+	}
+
+    public void SetDirection(float horizontalDirection, float verticalDirection)
+    {
+        if(Mathf.Abs(horizontalDirection) > Mathf.Abs(verticalDirection) && horizontalDirection > 0)
+        {
+            ResetAllTriggers();
+            animator.SetTrigger("isRight");
+        }
+        if (Mathf.Abs(horizontalDirection) > Mathf.Abs(verticalDirection) && horizontalDirection < 0)
+        {
+            ResetAllTriggers();
+            animator.SetTrigger("isLeft");
+        }
+        if (Mathf.Abs(horizontalDirection) < Mathf.Abs(verticalDirection) && verticalDirection < 0)
+        {
+            ResetAllTriggers();
+            animator.SetTrigger("isDown");
+        }
+        if (Mathf.Abs(horizontalDirection) < Mathf.Abs(verticalDirection) && verticalDirection > 0)
+        {
+            ResetAllTriggers();
+            animator.SetTrigger("isUp");
+        }
+    }
+
+    public void ResetAllTriggers()
+    {
+        animator.ResetTrigger("isRight");
+        animator.ResetTrigger("isUp");
+        animator.ResetTrigger("isDown");
+        animator.ResetTrigger("isLeft");
+    }
+}
